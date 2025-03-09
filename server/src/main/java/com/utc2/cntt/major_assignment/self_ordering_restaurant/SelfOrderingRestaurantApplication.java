4d299a8bfe08382cc65968277a439b9ec97df844
Xuan Diep <diepp031005@gmail.com>
2025-03-09 16:45:13 +0700

    init project

---
+++ server/src/main/java/com/utc2/cntt/major_assignment/self_ordering_restaurant/SelfOrderingRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.utc2.cntt.major_assignment.self_ordering_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfOrderingRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfOrderingRestaurantApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/utc2/cntt/major_assignment/self_ordering_restaurant/SelfOrderingRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utc2.cntt.major_assignment.self_ordering_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfOrderingRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
